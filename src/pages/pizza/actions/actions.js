57d1f5777d5b3e21040b91aedab5c8a71b07429e
Mozdolevskyi Maksym <72501713+MozdolevskyiMaksym@users.noreply.github.com>
2023-04-07 14:21:51 +0300

    FE-8: Handles exception using languages build in methods and methodology

--- src/pages/pizza/actions/actions.js
+++ src/pages/pizza/actions/actions.js
@@ -1,18 +1,30 @@
+/* eslint-disable no-console */
 import { all, fork, takeEvery, call, put } from 'redux-saga/effects';
 import { FETCH_PIZZA, FETCH_PIZZA_SUCCESS } from './action-types';
 import ApiService from '../../../shared/helpers/api-service/api-service';
+import { FetchPizzaError } from '../../../utils/custom-errors';
 
 export const fetchPizza = () => ({
   type: FETCH_PIZZA,
 });
 
 function* fetchPizzaWorker() {
-  const data = yield call(ApiService.load, 'dataPizza');
+  try {
+    const data = yield call(ApiService.load, 'dataPizza');
 
-  yield put({
-    type: FETCH_PIZZA_SUCCESS,
-    payload: { pizza: data },
-  });
+    yield put({
+      type: FETCH_PIZZA_SUCCESS,
+      payload: { pizza: data },
+    });
+  } catch (error) {
+    if (error instanceof FetchPizzaError) {
+      // Обработка конкретного типа ошибок FetchPizzaError
+      console.error('Ошибка при загрузке данных пиццы:', error.message);
+    } else {
+      // Общая обработка ошибок
+      console.error('Ошибка при загрузке данных пиццы:', error);
+    }
+  }
 }
 
 function* fetchPizzaWatcher() {
